f210ca1e3d9de720acea6aebbb5f6786faf0263c
erincherish <53274874+erincherish@users.noreply.github.com>
2021-06-13 23:12:01 -0700

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  title: 'Blog of Erin',
+  author: 'Erin Huang',
+  email: 'erincherish@gmail.com',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP']
